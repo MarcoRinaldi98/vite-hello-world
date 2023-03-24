b75cdee6682ffc88e9e9397bf315b292e0a272f8
Marco Rinaldi <marcorinaldi98@libero.it>
2023-03-24 15:46:18 +0100

    esercizio base completato

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
-<template></template>
+<template>
+  <AppTitle />
+</template>
 
 <script>
+import AppTitle from './components/AppTitle.vue';
 
+export default {
+
+  components: {
+    AppTitle
+  }
+
+}
 </script>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1> La mia prima app con Vite! </h1>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+h1 {
+    color: red;
+    text-align: center;
+    font-size: 70px;
+}
+</style>
